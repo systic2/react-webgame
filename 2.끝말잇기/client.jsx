4f734649f92ff7728d4ecdc2eb6b99be99fcad2e
systic <llsysticll@gmail.com>
2022-10-26 18:07:03 +0900

    commit 2022-10-26 리액트 2-5 완료

---
+++ 2.끝말잇기/client.jsx
@@ -0,0 +1,6 @@
+const React = require('react');
+const ReacDom = require('react-dom');
+
+const WordRelay = require('./WordRelay');
+
+ReactDOM.render(<WordRelay />, document.querySelector('#root'));
---
+++ 2.끝말잇기/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  name: 'wordrelay-setting',
+  mode: 'development', // 실서비스: production
+  devtool: 'eval',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
+  entry: {
+    app: ['./client'],
+  }, // 입력
+
+  module: {
+    rules: [{
+      test: /\.jsx?/,
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env', '@babel/preset-react'],
+        plugins: ['@babel/plugin-proposal-class-properties'],
+      }
+    }],
+  },
+
+  output: {
+    path: path.join(__dirname, 'dist'), // d:\react-webgame\
+    filename: 'app.js',
+  }, // 출력
+};
