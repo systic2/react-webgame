f8b0046c50e82cdfd7d8c7693c05fef9dfc086dc
systic <llsysticll@gmail.com>
2022-11-23 17:46:47 +0900

    commit 2022-11-23 리액트 6-6 완료

--- 6.로또추첨기/Lotto.jsx
+++ 6.로또추첨기/Lotto.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useRef, useState, useMemo, useCallback } from "react";
 import Ball from "./Ball";
 
 function getWinNumbers() {
@@ -17,81 +17,72 @@ function getWinNumbers() {
   return [...winNumbers, bonusNumber];
 }
 
-class Lotto extends Component {
-  state = {
-    winNumbers: getWinNumbers(), // 당첨 숫자들
-    winBalls: [],
-    bonus: null, // 보너스 공
-    redo: false,
-  };
+// Note
+// Hooks는 조건문 안에 절대 넣으면 안된다.
+// 함수나 반복문 안에도 웬만하면 넣지 마라.
 
-  timeouts = [];
+// useMemo: 복잡한 함수 결괏값(리턴값)을 기억
+// useRef: 일반 값을 기억
+const Lotto = () => {
+  const lottoNumbers = useMemo(() => getWinNumbers(), []);
+  const [winNumbers, setWinNumbers] = useState(lottoNumbers);
+  const [winBalls, setWinBalls] = useState([]);
+  const [bonus, setBonus] = useState(null);
+  const [redo, setRedo] = useState(false);
+  const timeouts = useRef([]);
 
-  runTimeouts = () => {
-    console.log('runTimeouts');
-    const { winNumbers } = this.state;
-    for (let i = 0; i < this.state.winNumbers.length - 1; i++) {
-      this.timeouts[i] = setTimeout(() => {
-        this.setState((prevState) => {
-          return {
-            winBalls: [...prevState.winBalls, winNumbers[i]],
-          };
-        });
+  // const mounted = useRef(false);
+  // useEffect(() => {
+  //   if (!mounted.current) {
+  //     mounted.current = true;
+  //   } else {
+  //     // ajax
+  //   }
+  // }, [바뀌는값]); // componentDidUpdate만, componentDidMount는 X
+
+  useEffect(() => {
+    for (let i = 0; i < winNumbers.length - 1; i++) {
+      timeouts.current[i] = setTimeout(() => {
+        setWinBalls((prevWinballs) => [...prevWinballs, winNumbers[i]]);
       }, (i + 1) * 1000);
     }
-    this.timeouts[6] = setTimeout(() => {
-      this.setState({
-        bonus: winNumbers[6],
-        redo: true,
-      });
+    timeouts.current[6] = setTimeout(() => {
+      setBonus(winNumbers[6]);
+      setRedo(true);
     }, 7000);
-  }
-
-  componentDidMount() {
-    console.log('didMount');
-    this.runTimeouts();
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log('didUpdate');
-    if (this.state.winBalls.length === 0) {
-      this.runTimeouts();
+    return () => { // return 은 componentWillUnMount를 수행
+      timeouts.current.forEach((v) => {
+        clearTimeout(v);
+      });
     }
-  }
+  }, [timeouts.current]) // 빈 배열이면 componentDidMount와 동일
+  // 배열에 요소가 있으면 componentDidMount랑 componentDidUpdate 둘 다 수행
 
-  componentWillUnmount() {
-    this.timeouts.forEach((v) => {
-      clearTimeout(v);
-    });
-  }
-
-  onClickRedo = () => {
+  // useCallback : 함수를 기억
+  // 자식컴포넌트에 props로 넘겨줄 때는 useCallback을 꼭 사용해야 한다.
+  const onClickRedo = useCallback(() => {
     console.log('onClickRedo');
-    this.setState({
-      winNumbers: getWinNumbers(), // 당첨 숫자들
-      winBalls: [],
-      bonus: null, // 보너스 공
-      redo: false,
-    });
-    this.timeouts = [];
-  };
+    console.log(winNumbers);
+    setWinNumbers(getWinNumbers());
+    setWinBalls([]);
+    setBonus(null);
+    setRedo(false);
+    timeouts.current = [];
+  }, [winNumbers]);
 
-  render() {
-    const { winBalls, bonus, redo } = this.state;
-    return (
-      <>
-        <div>당첨 숫자</div>
-        <div id="결과창">
-          {winBalls.map((v) => (
-            <Ball key={v} number={v} />
-          ))}
-        </div>
-        <div>보너스!</div>
-        {bonus && <Ball number={bonus} />}
-        {redo && <button onClick={this.onClickRedo}>한 번 더!</button>}
-      </>
-    );
-  }
+  return (
+    <>
+      <div>당첨 숫자</div>
+      <div id="결과창">
+        {winBalls.map((v) => (
+          <Ball key={v} number={v} />
+        ))}
+      </div>
+      <div>보너스!</div>
+      {bonus && <Ball number={bonus} />}
+      {redo && <button onClick={onClickRedo}>한 번 더!</button>}
+    </>
+  );
 }
 
 export default Lotto;
